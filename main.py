d2fd57c5308f0c056d7545a006559abd4f5f3129
bhavesh-bk <bhaveshbk@2003@gmail.com>
2025-07-30 09:24:41 +0530

    final commit

--- main.py
+++ main.py
@@ -10,3 +10,30 @@ turtle.shape(image)
 
 ans = screen.textinput(title="Guess the State", prompt="What's the another state's name?")
 
+import pandas as pd
+
+dt = pd.read_csv("50_states.csv")
+states = dt["state"].to_list()
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+
+state_dt = dt[dt.state == states]
+
+row = state_dt.iloc[0]
+t.goto(int(row.x), int(row.y))
+
+t.write(ans)
+
+guesses = []
+
+while len(guesses) < 50:
+    ans = screen.textinput(title=f"{len(guesses)}/50 States correct", prompt="What's another state's name?")
+    ans = ans.title()
+    if ans in states and ans not in guesses:
+        guesses.append(ans)
+        state_dt = dt[dt.state == ans]
+        t.goto(int(state_dt.x.item()), int(state_dt.y.item()))
+        t.write(ans)
+# ans = ans.title()
